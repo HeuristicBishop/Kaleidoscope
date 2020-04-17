c3e8d87f29be1723db0810cdf154319dac1c5a1d
HeuristicBishop <HeuristicBishop@outlook.com>
2020-04-16 23:25:16 -0400

    fixed a silly mistake

--- examples/Devices/sherbet/sherbet/sherbet.ino
+++ examples/Devices/sherbet/sherbet/sherbet.ino
@@ -50,6 +50,7 @@ KEYMAPS(
   ,Key_Tab         ,Key_Q       ,Key_W ,Key_E ,Key_R ,Key_T
   ,Key_CapsLock    ,Key_A       ,Key_S ,Key_D ,Key_F ,Key_G
   ,Key_LeftShift   ,Key_Z       ,Key_X ,Key_C ,Key_V ,Key_B
+)
 );
 /* *INDENT-ON* */
 
